1069ba2b8f56f1bd817e385d4daf010e9ee6f83c
cmonllor <cmonllor@gmail.com>
2024-05-01 20:37:07 +0200

    Node JS CLI frontend updated

--- js/index.js
+++ js/index.js
@@ -4,13 +4,119 @@ const ethers = require('ethers')
     const start = async()=>{
         console.log('CCIP Messenger started!')
 
+
+
         const arg  = parseInt( process.argv[2] )
 
+        require('dotenv').config()
+        const fs = require('fs')
+        const path = require('path')
+
+        chainData  = JSON.parse( fs.readFileSync( path.resolve(__dirname, 'chainData.json') ) )
+
+        
         if( arg == 0 ){
-            console.log('TODO: fuji')
+            console.log('Connecting to Avalanche Fuji Testnet...')
+            const rpc = process.env.FUJI_RPC_URL;
+            const prik = process.env.FUJI_ACCOUNT_PRIVATE_KEY
+
+            const messengerAddress = chainData.chains[0].messengerAddress;
+
+            const provider = new ethers.providers.JsonRpcProvider(rpc);
+            const wallet = new ethers.Wallet(prik, provider);
+
+            const abi = 
+            [
+                "function sendMessage(uint64,address,bytes)",
+                "event MessageSent(bytes32,uint64,address,bytes,address,uint256)",
+                "event MessageReceived(bytes32,uint64,address,bytes)",
+                "event MessageAcknowledged(bytes32,uint64,address,bytes)",
+                "event ErrorReceived(bytes32,uint64,address,bytes)"
+            ]
+
+            //subscribe to events
+            const msgrCtrt = new ethers.Contract(messengerAddress, abi, provider);
+            msgrCtrt.on('MessageSent', (msgId, chain, dest, msg, feeToken, fee) => {
+                console.log('Message [', msgId, '] to chain <', chain, '>:<', dest, '> sent.'); 
+                console.log('Message: ', msg)
+            })
+            msgrCtrt.on('MessageReceived', (msgId, chain, src, msg) => {
+                console.log('Message [', msgId, '] from chain <', chain, '> received from <', src, '>');
+                console.log("Message:", msg)
+            })
+            msgrCtrt.on('MessageAcknowledged', (msgId, chain, dest, msg) => {
+                console.log('Message [', msgId, '] to chain <', chain, '> to <', dest, '> acknowledged');
+                console.log("Message:", msg)
+            })
+            msgrCtrt.on('ErrorReceived', (msgId, chain, dest, msg) => {
+                console.log('Message [', msgId, '] to chain <', chain, '> to <', dest, '> errored');
+                console.log("Message:", msg)
+            })
+
+            const chId = chainData.chains[1].chainlinkId
+            const dest = chainData.chains[1].messengerAddress
+            const msg = ethers.utils.toUtf8Bytes('Hola Don Pepito!')
+            const encodedMsg = ethers.abi.encode(['bytes'], [msg])
+            msgrCtrt.sendMessage(chId, dest, encodedMsg).then( (tx) => {
+                console.log('Transaction with Message sent. Tx:', tx.hash)
+            }).catch( (err) => {
+                console.log('Error sending message:', err)
+            })
+
         }
         else if( arg == 1 ){
+            const prik = process.env.SEPOLIA_ACCOUNT_PRIVATE_KEY
             console.log('TODO: sepolia')
+            console.log('Connecting to Ethereum Sepolia Testnet...')
+            const rpc = process.env.SEPOLIA_RPC_URL;
+
+            const messengerAddress = chainData.chains[1].messengerAddress;
+
+            const provider = new ethers.providers.JsonRpcProvider(rpc);
+            const wallet = new ethers.Wallet(prik, provider);
+
+            const abi = 
+            [
+                "function sendMessage(uint64,address,bytes)",
+                "event MessageSent(bytes32,uint64,address,bytes,address,uint256)",
+                "event MessageReceived(bytes32,uint64,address,bytes)",
+                "event MessageAcknowledged(bytes32,uint64,address,bytes)",
+                "event ErrorReceived(bytes32,uint64,address,bytes)"
+            ]
+
+            //subscribe to events
+            const msgrCtrt = new ethers.Contract(messengerAddress, abi, provider);
+            msgrCtrt.on('MessageSent', (msgId, chain, dest, msg, feeToken, fee) => {
+                console.log('Message [', msgId, '] to chain <', chain, '>:<', dest, '> sent.'); 
+                console.log('Message: ', msg)
+            })
+            msgrCtrt.on('MessageReceived', (msgId, chain, src, msg) => {
+                console.log('Message [', msgId, '] from chain <', chain, '> received from <', src, '>');
+                console.log("Message:", msg)
+            })
+            msgrCtrt.on('MessageAcknowledged', (msgId, chain, dest, msg) => {
+                console.log('Message [', msgId, '] to chain <', chain, '> to <', dest, '> acknowledged');
+                console.log("Message:", msg)
+
+                const decodedMsg = ethers.utils.defaultAbiCoder.decode(['bytes'], msg)
+                console.log('Decoded Message:', decodedMsg[0].toString())
+                const chId = chainData.chains[1].chainlinkId
+                const dest = chainData.chains[1].messengerAddress
+                const msg = ethers.utils.toUtf8Bytes('Hola Don José!')
+                const encodedMsg = ethers.abi.encode(['bytes'], [msg])
+                msgrCtrt.sendMessage(chId, dest, encodedMsg).then( (tx) => {
+                    console.log('Transaction with Message sent. Tx:', tx.hash)
+                }).catch( (err) => {
+                    console.log('Error sending message:', err)
+                })
+            })
+            msgrCtrt.on('ErrorReceived', (msgId, chain, dest, msg) => {
+                console.log('Message [', msgId, '] to chain <', chain, '> to <', dest, '> errored');
+                console.log("Message:", msg)
+            })
+
+            
+
         }
     }
 
